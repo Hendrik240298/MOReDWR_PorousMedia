3dd66d719777f5a4c75b2b47b4a271ff44ad4745
Julian Roth <julian.roth1998@gmx.de>
2023-09-13 15:50:36 +0200

    Added more debugging and tried out other parameters

--- Experimental/fenics_3d_footing_problem_backward_euler.py
+++ Experimental/fenics_3d_footing_problem_backward_euler.py
@@ -1,5 +1,6 @@
 import dolfin
 import os
+from ufl import replace
 
 ##############
 # PARAMETERS #
@@ -9,19 +10,17 @@ M_biot = 1.75e+7 #2.5e+12
 c_biot = 1.0 / M_biot
 
 # alpha_biot = b_biot = Biot's modulo
-alpha_biot = 1.0
+alpha_biot = 0.0 #1.0 # TODO: change back to 1.
 viscosity_biot = 1.0e-3
 K_biot = 1.0e-6
-density_biot = 1.0
 
 # Traction
 traction_x_biot = 0.0
 traction_y_biot = 0.0 #-1.0e+7
-traction_z_biot = 0.1
+traction_z_biot = 0.00001
 
 # Solid parameters
-density_structure = 1.0
-youngs_modulus_E = 3.0e+4
+youngs_modulus_E = 1.0e+8 #3.0e+4 # TODO: change back to 3.0e+4
 poisson_ratio_nu = 0.2
 lame_coefficient_mu = youngs_modulus_E / (2. * (1. + poisson_ratio_nu))
 lame_coefficient_lambda = (2. * poisson_ratio_nu * lame_coefficient_mu) / (1.0 - 2. * poisson_ratio_nu)
@@ -36,21 +35,15 @@ set_log_active(False) # turn off FEniCS logging
 
 mesh = BoxMesh(Point(-32.,-32.,0.), Point(32.,32.,64.), 16, 16, 16)
 dim = mesh.geometry().dim()
-# plot(mesh)
-# plt.show()
-
-# times = [1000., 5000., 10000., 100000.,  500000., 5000000.]
-# x_linspace = np.linspace(0., 100., 16)
-# solutions = {"u_x": {}, "p": {}}
 
 cpu_start_time = time.time()
 
 # start time
 t = 0.
 # end time
-T = 0.5 #5.0e+6
+T = 0.5
 # time step size
-k = 0.001 #0.01 #1000.
+k = 0.000000001 #0.001 #0.01
 # time step number
 time_step_number = 0
 
@@ -61,9 +54,13 @@ element = {
 V = FunctionSpace(mesh, MixedElement(*element.values()))
 print(f"Number of spatial DoFs: {V.dim()} ({V.sub(0).dim()} + {V.sub(1).dim()})")
 
-compression = CompiledSubDomain("near(x[2], 64.) && x[0] >= -16. && x[0] <= 16. && x[1] >= -16. && x[1] <= 16. && on_boundary")
+# compression = CompiledSubDomain("near(x[2], 64.) && x[0] >= -16. && x[0] <= 16. && x[1] >= -16. && x[1] <= 16. && on_boundary")
+# dirichlet = CompiledSubDomain("near(x[2], 0.) && on_boundary")
+# neumann = CompiledSubDomain("( near(x[0], -32.) || near(x[0], 32.) || near(x[1], -32.) || near(x[1], 32.) || (near(x[2], 64.) && (x[0] <= -16. || x[0] >= 16. || x[1] <= -16. || x[1] >= 16.) ) ) && on_boundary")
+
+compression = CompiledSubDomain("near(x[2], 64.) && x[0] >= 0. && x[0] <= 32. && x[1] >= 0. && x[1] <= 32. && on_boundary")
 dirichlet = CompiledSubDomain("near(x[2], 0.) && on_boundary")
-neumann = CompiledSubDomain("( near(x[0], -32.) || near(x[0], 32.) || near(x[1], -32.) || near(x[1], 32.) || (near(x[2], 64.) && (x[0] <= -16. || x[0] >= 16. || x[1] <= -16. || x[1] >= 16.) ) ) && on_boundary")
+neumann = CompiledSubDomain("( near(x[0], -32.) || near(x[0], 32.) || near(x[1], -32.) || near(x[1], 32.) || (near(x[2], 64.) && (x[0] <= 0. || x[1] <= 0.) ) ) && on_boundary")
 
 facet_marker = MeshFunction("size_t", mesh, 1)
 facet_marker.set_all(0)
@@ -80,7 +77,8 @@ ds_neumann = Measure("ds", subdomain_data=facet_marker, subdomain_id=3)
 bc_down_x = DirichletBC(V.sub(0).sub(0), Constant(0.), dirichlet) # dirichlet: u_x = 0
 bc_down_y = DirichletBC(V.sub(0).sub(1), Constant(0.), dirichlet) # dirichlet: u_y = 0
 bc_down_z = DirichletBC(V.sub(0).sub(2), Constant(0.), dirichlet) # dirichlet: u_z = 0
-bcs = [bc_down_x, bc_down_y, bc_down_z]
+bc_compression_p = DirichletBC(V.sub(1), Constant(0.), compression) # dirichlet: p = 0
+bcs = [bc_down_x, bc_down_y, bc_down_z, bc_compression_p]
 
 # zero initial condition for u and p
 U_0 = Constant((0.,)*(dim+1))
@@ -101,9 +99,11 @@ def stress(u):
     return lame_coefficient_lambda*div(u)*Identity(dim) + lame_coefficient_mu*(grad(u) + grad(u).T)
 
 n = FacetNormal(mesh)
-A_u = inner(stress(u), grad(phi_u))*dx - alpha_biot*inner(p*Identity(dim), grad(phi_u))*dx #+ alpha_biot*inner(p*n, phi_u)*ds_compression + alpha_biot*inner(p*n, phi_u)*ds_neumann
-A_p = alpha_biot*div(u)*phi_p*dx + k*(K_biot/viscosity_biot)*inner(grad(p), grad(phi_p))*dx + c_biot*p*phi_p*dx
-L = Constant(-traction_z_biot)*Phi[2]*ds_compression + alpha_biot*div(u_n)*phi_p*dx + c_biot*p_n*phi_p*dx
+A_u = inner(stress(u), grad(phi_u))*dx - Constant(alpha_biot)*inner(p*Identity(dim), grad(phi_u))*dx
+ #+ alpha_biot*inner(p*n, phi_u)*ds_compression + alpha_biot*inner(p*n, phi_u)*ds_neumann
+A_p = Constant(alpha_biot)*div(u)*phi_p*dx + k*(K_biot/viscosity_biot)*inner(grad(p), grad(phi_p))*dx + c_biot*p*phi_p*dx
+L = inner(Constant((traction_x_biot, traction_y_biot, traction_z_biot)), phi_u)*ds_compression + Constant(alpha_biot)*div(u_n)*phi_p*dx + c_biot*p_n*phi_p*dx
+# Constant(traction_z_biot)*phi_u[2]*ds_compression
 
 # Time-stepping
 Uh = Function(V) # Uh = U_{n+1}: current solution
@@ -126,7 +126,28 @@ while(t+k <= T+1e-8):
     
     vtk_displacement = File(f"{folder}/displacement_{str(time_step_number)}.pvd")
     vtk_pressure = File(f"{folder}/pressure_{str(time_step_number)}.pvd")
+
+    # FOR DEBUGGING:
+    # Uh.vector().set_local(assemble(Constant(traction_z_biot)*phi_u[2]*ds_compression).get_local())
+    # Uh.vector()[:] = np.array(assemble(Constant(traction_z_biot)*phi_u[2]*ds_compression))[:]
+    # print(f"    max = {np.max(np.array(assemble(Constant(traction_z_biot)*phi_u[2]*ds_compression)))}")
+    # print(f"    u_NEW(1,1,64) = {Uh(1.,1.,64.)}")
+
     _u, _p = Uh.split()
+
+    print("Residual:", np.linalg.norm(np.array(assemble(
+        inner(stress(_u), grad(phi_u))*dx - Constant(alpha_biot)*inner(_p*Identity(dim), grad(phi_u))*dx \
+        + Constant(alpha_biot)*div(_u)*phi_p*dx + k*(K_biot/viscosity_biot)*inner(grad(_p), grad(phi_p))*dx + c_biot*_p*phi_p*dx \
+        - inner(Constant((traction_x_biot, traction_y_biot, traction_z_biot)), phi_u)*ds_compression - Constant(alpha_biot)*div(u_n)*phi_p*dx - c_biot*p_n*phi_p*dx
+    ))))
+
+    print("Residual 2: ", norm(assemble(A_u) * Uh.vector() + assemble(A_p) * Uh.vector() - assemble(L)))
+
+    residual = assemble(A_u) * Uh.vector() + assemble(A_p) * Uh.vector() - assemble(L)
+    for bc in bcs:
+        bc.apply(residual)
+    print("Residual 3: ", norm(residual))
+        #-A_u.replace({u: _u, p: _p})-A_p.replace({u: _u, p: _p})+L))))
     _u.rename("displacement", "solution")
     _p.rename("pressure", "solution")
     vtk_displacement.write(_u)
